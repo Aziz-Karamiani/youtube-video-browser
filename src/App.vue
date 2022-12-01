649bbd0fc73155622de34cb05d152d6fcc41dd6b
a.karamiani <aziz.karamiani@gmail.com>
2022-12-01 22:47:22 +0330

    Clean up project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    It Works!.
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
